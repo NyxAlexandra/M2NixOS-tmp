240629d2871164ad8e43a37c4613cba42c05e638
NyxAlexandra <nyxalexandra@proton.me>
2023-03-17 21:16:21 -0400

    set a cursor

--- modules/home-manager/alex/default.nix
+++ modules/home-manager/alex/default.nix
@@ -11,5 +11,15 @@
       gnomeExtensions.blur-my-shell
       gnomeExtensions.just-perfection
     ];
+
+    pointerCursor = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+      size = 24;
+      x11 = {
+        enable = true;
+        defaultCursor = "Adwaita";
+      };
+    };
   };
 }
